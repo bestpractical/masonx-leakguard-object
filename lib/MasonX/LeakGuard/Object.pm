9d6b7da048308c8725aa9fccafe41ea9fb02d99f
Ruslan Zakirov <ruz@bestpractical.com>
2009-10-10 01:28:14 +0400

    complete refactor to allow easier on_leak overriding

--- lib/MasonX/LeakGuard/Object.pm
+++ lib/MasonX/LeakGuard/Object.pm
@@ -36,53 +36,127 @@ MasonX::LeakGuard::Object - report memory leaks (objects) per request
 
 =head1 DESCRIPTION
 
-This is plugin for L<HTML::Mason> applications that helps you find
-memory leaks in OO applications. It uses L<Devel::LeakGuard::Object>
+This is plugin for L<HTML::Mason> based applications that helps you
+find memory leaks in OO code. It uses L<Devel::LeakGuard::Object>
 framework for that.
 
-Leaks are reported using perl's C<warn> function. Report is very simple,
-shows request path, arguments (using L<Data::Dumper>) and counts
-of objects that leaked with groupping by class.
+=head1 OPTIONS
 
-It's possible that false positives to be reported, for example if
-a compontent has ONCE block where you cache some values. Most caches
-will generate false positive reports, but it's possible to use
-options of L<function leakguard in Devel::LeakGuard::Object|Devel::LeakGuard::Object/leakguard>.
-Look at L</SYNOPSIS> for details.
+It's possible to use all options leakguard function in
+L<Devel::LeakGuard::Object> supports. Look at L</SYNOPSIS> for example.
+
+=head2 on_leak
+
+on_leak option can be used to customize report or redirect to custom
+log, for example:
+
+    %MasonX::LeakGuard::Object::OPTIONS = (
+        on_leak => sub {
+            my ($report, %args) = @_;
+            MyApp->logger->error( $args{'message'} );
+        },
+    );
+
+First argument is the report structure (read more on it in 
+L<Devel::LeakGuard::Object>). To make life easier more data
+is passed into your function as a hash. The hash contains:
+
+=over 4
+
+=item formatted - formatted report, a text table.
+
+=item message - formatted default message containg description,
+request path, request arguments and report. For example:
+
+    Leak(s) found after request to '/index.html' with $ARGS1 = [
+               'argument',
+               'value',
+             ];
+    Leaked objects:
+      Class          Before  After  Delta
+      MyApp::Class        0      1      1
+
+=item path - request path.
+
+=item arguments - request arguments, array reference.
+
+=back
+
+=head1 FALSE POSITIVES
+
+It's possible that false positives are reported, for example if
+a compontent has ONCE block where you cache some values on first
+request. Most caches will generate false positive reports, but
+it's possible to use options of L<function leakguard in Devel::LeakGuard::Object|Devel::LeakGuard::Object/leakguard>. Look at L</SYNOPSIS> for
+example and L</OPTIONS> for additional details.
 
 To avoid many false positives the module as well B<ignores> first
 request to a path.
+
+Sure it doesn't protect you from all cases.
 
 =cut
 
 sub start_request_hook {
     my ($self, $context) = @_;
-    $self->{'path'} = $context->request->request_comp->path;
-    if ( $BEEN_IN{ $self->{'path'} } ) {
-        $self->{'args'} = Dumper( scalar $context->args );
-        $self->{'state'} = Devel::LeakGuard::Object::State->new(
-            %OPTIONS
-        );
-    }
-}
 
-sub end_request_hook {
-    my ($self, $context) = @_;
-
-    my $path = $self->{'path'};
+    my $path = $context->request->request_comp->path;
     unless ( $BEEN_IN{ $path } ) {
         $BEEN_IN{ $path } = 1;
         return;
     }
 
-    my $state = $self->{'state'};
-    my $args = $self->{'args'};
-
-    my $old_sub = $state->{'on_leak'};
-    $state->{'on_leak'} = sub {
-        warn "Leak in $path with args '$args'";
-        return $old_sub->(@_)
+    my @request_args = @{ scalar $context->args };
+    my $base_handler = sub {
+        my $report = shift;
+        my $formatted = Devel::LeakGuard::Object::State->_fmt_report(
+            $report
+        );
+        local $Data::Dumper::Varname = 'ARGS';
+        my $msg =
+            "Leak(s) found after request to '$path'"
+            ." with ". Dumper(\@request_args)
+            ."Leaked objects:\n". $formatted;
+        return {
+            report    => $report,
+            formatted => $formatted,
+            message   => $msg,
+            path      => $path,
+            arguments => \@request_args,
+        };
     };
+
+    my $leak_handler;
+    if ( $OPTIONS{'on_leak'} ) {
+        if ( ref $OPTIONS{'on_leak'} eq 'CODE' ) {
+            $leak_handler = sub {
+                my $tmp = $base_handler->(@_);
+                return $OPTIONS{'on_leak'}->(
+                    delete $tmp->{'report'},
+                    %$tmp,
+                );
+            };
+        } elsif ( $OPTIONS{'on_leak'} eq 'die' ) {
+            $leak_handler = sub { die $base_handler->(@_)->{'message'} };
+        } elsif ( $OPTIONS{'on_leak'} eq 'warn' ) {
+            $leak_handler = sub { warn $base_handler->(@_)->{'message'} };
+        } else {
+            die "Incorrect on_leak option";
+        }
+    } else {
+        $leak_handler = sub { warn $base_handler->(@_)->{'message'} };
+    }
+
+    $self->{'state'} = Devel::LeakGuard::Object::State->new(
+        %OPTIONS, on_leak => $leak_handler,
+    );
+}
+
+sub end_request_hook {
+    my ($self, $context) = @_;
+
+    my $state = delete $self->{'state'}
+        or return;
     $state->done;
 }
 
